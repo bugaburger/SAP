44367dd89c3abcc0d03961ba292fdf15809e157b
bugaburger <iburgerb@gmail.com>
2024-05-16 00:42:10 +0000

    creacion primer commit

---
+++ webapp/test/integration/opaTests.qunit.js
@@ -0,0 +1,7 @@
+/* global QUnit */
+
+sap.ui.require(["invoicesprueba/invoicesprueba/test/integration/AllJourneys"
+], function () {
+	QUnit.config.autostart = false;
+	QUnit.start();
+});
---
+++ webapp/test/unit/AllTests.js
@@ -0,0 +1,5 @@
+sap.ui.define([
+	"invoicesprueba/invoicesprueba/test/unit/controller/View1.controller"
+], function () {
+	"use strict";
+});
